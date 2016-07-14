5669ac35cd47082acfd08b5e24ce3f552cdf5b8b
ou5y <977945732@qq.con>
2016-07-14 17:14:37 +0800

    首次提交

---
+++ Duang/WebContent/readme.md
@@ -0,0 +1,2 @@
+## git使用
+### http://www.cnblogs.com/draem0507/archive/2013/01/14/2859954.html
---
+++ Duang/src/org/duang/Test.java
@@ -0,0 +1,9 @@
+package org.duang;
+
+import org.apache.struts2.convention.annotation.Actions;
+import org.hibernate.Hibernate;
+
+@Actions
+public class Test {
+Hibernate hibernate  = null;
+}
