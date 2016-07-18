43e21f061c7593d512163fb84537743f280d43fe
baipan_home <977945732@qq.com>
2016-07-18 23:27:44 +0800

    7月18日晚。在家中调试框架

---
+++ Duang/src/org/duang/action/sys/JumpAction.java
@@ -0,0 +1,48 @@
+package org.duang.action.sys;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.InterceptorRef;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Namespaces;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**   
+ * 专用于跳转的action
+ * @ClassName:  JumpAction   
+ * @Description:TODO(这里用一句话描述这个类的作用)   
+ * @author 白攀
+ * @date 2016年7月18日 下午10:05:36      
+ */  
+@Scope(value="singleton", proxyMode=ScopedProxyMode.NO)
+@InterceptorRef("defaultStack")
+@Namespaces(@Namespace("/"))
+@Action(value="jumpaction", results={
+		@Result(name="testAdd",type="dispatcher",location="WEB-INF/page/test/add.jsp"),
+		@Result(name="error",type="dispatcher",location="error.jsp")
+})
+public class JumpAction extends ActionSupport{
+
+	/**   
+	 * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)   
+	 */   
+	private static final long serialVersionUID = 1528753327964698159L;
+
+	/**   
+	 * 去测试的添加页面
+	 * @Title: toTestAdd   
+	 * @Description: TODO(这里用一句话描述这个方法的作用)   
+	 * @param: @return  
+	 * @author 白攀    
+	 * @date 2016年7月18日 下午10:06:46
+	 * @return: String      
+	 * @throws   
+	 */  
+	public String toTestAdd(){
+		return "testAdd";
+	}
+
+}
