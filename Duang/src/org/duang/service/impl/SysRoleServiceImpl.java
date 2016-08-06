2997607b6062c23175138b3500a96efcf53d8e2e
baipan_home <977945732@qq.com>
2016-08-06 19:58:27 +0800

    增添CommonInterface常用方法

--- Duang/src/org/duang/service/impl/SysRoleServiceImpl.java
+++ Duang/src/org/duang/service/impl/SysRoleServiceImpl.java
@@ -285,4 +285,88 @@ public class SysRoleServiceImpl implements SysRoleService{
 		return dao.executeSql(sqls);
 	}
 
+
+	/**
+	 * 通过属性与条件值删除实体数据
+	 * @param t  实体对象
+	 * @return   是否删除成功
+	 */
+	public boolean deleteEntity(String property, Object val) throws Exception{
+		return dao.deleteEntity(property, val);
+	}
+
+
+	/**
+	 * 根据datas数据集和条件属性与值来修改实体数据
+	 * @param datas
+	 * @param property
+	 * @param value
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean updateEntity(Map<String, Object> datas, String property, Object value) throws Exception{
+		return dao.updateEntity(datas, property, value);
+	}
+
+
+	/**
+	 * 根据datas数据集和conds条件集来修改实体数据
+	 * @param datas
+	 * @param conds
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean updateEntity(Map<String, Object> datas, Map<String, Object> conds) throws Exception{
+		return dao.updateEntity(datas, conds);
+	}
+
+
+	/**
+	 * 通过属性与值获取操作实体类
+	 * @param property 属性
+	 * @param value 值
+	 * @return
+	 * @throws Exception
+	 */
+	public SysRole findEntity(String property, Object value) throws Exception{
+		return dao.findEntity(property, value);
+	}
+
+
+	/**
+	 * 通过属性与值的集合获取操作实体类
+	 * @param params 属性与值的集合
+	 * @return
+	 * @throws Exception
+	 */
+	public SysRole findEntity(Map<String, Object> params) throws Exception{
+		return dao.findEntity(params);
+	}
+
+
+	/**
+	 * 根据Hql语句查询
+	 * @param hql hql语句
+	 * @param page 是否分页          null表示不分页
+	 * @param params 写法只有一种: "张三，男，24岁"
+	 * @return
+	 * @throws Exception
+	 */
+	public List<SysRole> queryByHQL(String hql, PageUtil<SysRole> page, Object... params) throws Exception{
+		return dao.queryByHQL(hql, page, params);
+	}
+
+
+	/**
+	 * 根据Sql语句查询
+	 * @param sql   sql语句
+	 * @param page  是否分页          null表示不分页
+	 * @param params 写法有俩；A:"张三，男，24岁"、   B:"Object[]{'name','张三'}，Object[]{'sex','男'}"
+	 * @return
+	 * @throws Exception
+	 */
+	public List<SysRole> queryBySQL(String sql, PageUtil<SysRole> page, Object... params) throws Exception{
+		return dao.queryBySQL(sql, page, params);
+	}
+
 }
