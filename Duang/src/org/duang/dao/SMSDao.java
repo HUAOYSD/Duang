d2e20457120d1e6d64938a6f5fddb37c35d50cbf
AllenLeeY <lyh19891023@163.com>
2016-09-19 11:46:59 +0800

    短信Dao

--- Duang/src/org/duang/dao/SMSDao.java
+++ Duang/src/org/duang/dao/SMSDao.java
@@ -4,7 +4,7 @@ import org.duang.baseInterface.CommonInterface;
 import org.duang.entity.SMS;
 /**
  * 短信Dao接口
- * @ClassName:  ApplyLoanInfoDao   
+ * @ClassName:  SMSDao   
  * @Description:TODO(这里用一句话描述这个类的作用)   
  * @author LiYonghui
  * @date 2016年9月19日 上午10:28:35
